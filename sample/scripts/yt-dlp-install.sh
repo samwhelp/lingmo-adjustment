43894f97fb0df11a65e30787f749e9f25fa17875
samwhelp <samwhelp@users.noreply.github.com>
2024-09-29 18:16:04 +0800

    fix

--- sample/scripts/yt-dlp-install.sh
+++ sample/scripts/yt-dlp-install.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 
+
+
+##
+## package install
+##
+
+sudo apt-get install yt-dlp
+
+
+
+
+##
+## manual install
+##
+
 mkdir -p "${HOME}/.local/bin"
 
 wget -c 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp' -O "${HOME}/.local/bin/yt-dlp"
