d2a0f6a9755abc5346de3567d08c13516c79b84d
samwhelp <samwhelp@users.noreply.github.com>
2024-10-24 21:31:14 +0800

    fix

--- sample/develop/build-package/helper/ext/master/base.sh
+++ sample/develop/build-package/helper/ext/master/base.sh
@@ -8,11 +8,16 @@ master_var_init () {
 
 
 	return 0
+
 }
+
 
 master_var_dump () {
 
 	is_not_debug && return 0
+
+
+
 
 	util_debug_echo
 	util_debug_echo "################################################################################"
@@ -20,6 +25,21 @@ master_var_dump () {
 	util_debug_echo "##"
 	util_debug_echo
 
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Config"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "_Conf_Repo_Name_=${_Conf_Repo_Name_}"
+	util_debug_echo "_Conf_Repo_Repository_=${_Conf_Repo_Repository_}"
+	util_debug_echo "_Conf_Repo_Tag_=${_Conf_Repo_Tag_}"
+	util_debug_echo "_Conf_Repo_UseBranchType_=${_Conf_Repo_UseBranchType_}"
+	util_debug_echo "_Conf_Repo_BuildType_=${_Conf_Repo_BuildType_}"
+	util_debug_echo
 
 
 
